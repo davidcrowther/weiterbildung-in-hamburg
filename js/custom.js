432c4f58e5584de841705c2598644ff860ee608a
dronni3de <dronni3de@gmail.com>
2015-08-18 16:21:01 +0300

    init

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+   $('section.page-header').anystretch("img/mountains.jpg", {speed: 150}); 
+   $('article.yellow-section').anystretch("img/yellow-bg.png", {speed: 150}); 
+   $('article.evening').anystretch("img/bg-2.jpg", {speed: 150}); 
+
+   $("a.answer-link").popover({
+    html: true,
+    placement: 'left auto',
+    animation: true ,
+    // content: '<ul class="list-group"> <li class="list-group-item">There is</li> <li class="list-group-item">It is</li> <li class="list-group-item">There are</li> <li class="list-group-item">There be</li> </ul>',
+    trigger: 'click'
+   });
+
+   $(document).on('click', 'a.answer-link', function(event) {
+     event.preventDefault();
+   });
+
+   $(document).on('click', '.popover-content ul li', function(event) {
+      var this_text = $(this).html();
+      var this_id = $(this).parent().parent().parent().find('h3.popover-title').text();
+      $(this_id).text(this_text);
+      $(this).parent().parent().parent().removeClass('in').css('display','none');
+      return false;        
+   });
+
+   $(document).on('click', 'article.submit-form form .btn', function(event) {
+    $('.hidden-content').removeClass('hide');
+    $('.visible-content').addClass('hide');
+   });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+});
